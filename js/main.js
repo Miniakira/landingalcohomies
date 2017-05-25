aaf1271225255f43397beddc1b56c9efe098aeac
Megaakira <Akiiira@gmail.com>
2017-05-25 10:54:18 +0200

    First commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+/*$(document).ready(function () {
+  $(".linkblock").hover( function(){
+    $(this).addClass("linkblockBlack");
+    $(this).find("a").addClass("textWhite");
+      }, function () {
+      $(this).removeClass('linkblockBlack');
+      $(this).find("a").addClass("textWhite");
+  });
+});
+*/
